6344669c100ec463602d2434d9e2be59fff3ad6f
ekuiwag <kui.k.wang@ericsson.com>
2018-11-08 17:54:15 +0800

    [Java8] Add lambda map test.

--- src/main/java/com/mymaven/java8/lambda/LambdaTestA.java
+++ src/main/java/com/mymaven/java8/lambda/LambdaTestA.java
@@ -1,6 +1,15 @@
 package com.mymaven.java8.lambda;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.testng.annotations.Test;
 
 public class LambdaTestA {
 
+    @Test
+    public void mapTest() {
+        List<Double> cost = Arrays.asList(10.0, 20.0, 30.0);
+        cost.stream().map(x -> x + x * 0.05).forEach(x -> System.out.println(x));
+    }
 }
